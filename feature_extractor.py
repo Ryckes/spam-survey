7c37c10f9ec2ef72e47f65c8cb6c5f58afa4998d
Ryckes <kevin.mnmn@gmail.com>
2016-04-30 18:28:14 +0200

    Add sentence-related features and Yule's K measure

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,6 +4,8 @@ from copy import copy
 import nltk
 from nltk.tokenize import word_tokenize
 
+import itertools
+
 def downloadNLTKIfNecessary():
     try:
         word_tokenize('')
@@ -12,6 +14,31 @@ def downloadNLTKIfNecessary():
         nltk.download('punkt')
 
 class FeatureExtractor:
+
+    DEFAULT_END_OF_SENTENCE_SYMBOLS = [',', '.', '!', '?', ':', ';']
+
+    def __init__(self):
+        self.endOfSentenceSymbols = self.DEFAULT_END_OF_SENTENCE_SYMBOLS
+
+    def getSentences(self, tokens):
+        sentences = [[]]
+
+        for token in tokens:
+            word = False
+            for char in token:
+                if char not in self.endOfSentenceSymbols:
+                    # It's a word
+                    word = True
+                    sentences[-1].append(token)
+                    break
+            if not word:
+                if len(sentences[-1]) > 0 or len(sentences) == 1:
+                    sentences[-1].append(token)
+                    sentences.append([])
+                else:
+                    sentences[-2].append(token)
+
+        return filter(lambda x: len(x) > 0, sentences)
 
     def getAverageWordLength(self, tokens):
         if len(tokens) == 0:
@@ -27,8 +54,35 @@ class FeatureExtractor:
         self.totalAlpha = sum([sum([1 if char.isalpha() else 0 for char in token]) for token in tokens])
         return self.totalAlpha / float(sum([len(token) for token in tokens]))
 
+    def getNumPunctuationCharacters(self, tokens):
+        punctuationCharacters = self.endOfSentenceSymbols
+        return sum([1 if char in punctuationCharacters else 0
+                    for token in tokens for char in token])
+
+    def getYuleKMeasure(self, tokens):
+        textDictionary = {}
+        for token in tokens:
+            try:
+                textDictionary[token] += 1
+            except KeyError:
+                textDictionary[token] = 1
+
+        # M1 is the number of all word forms a text consists of.
+        M1 = len(textDictionary)
+
+        # M2 is the sum of the products of each observed frequency to
+        # the power of two and the number of word types observed with
+        # that frequency.
+        M2 = sum([(freq ** 2) * len(list(wordsWithThisFreq)) for
+                  freq, wordsWithThisFreq in itertools.groupby(sorted(textDictionary.values()))])
+
+        try:
+            return 10000 * (M2 - M1) / float(M1 ** 2)
+        except ZeroDivisionError:
+            return 0
+
     def tokenize(self, string):
-        string = unicode(string, 'ascii', 'replace')
+        string = unicode(string, 'ascii', 'replace').lower()
         return word_tokenize(string)
 
     def process(self, mail):
@@ -47,7 +101,23 @@ class FeatureExtractor:
         featureDict['subject'] = self.tokenize(featureDict['subject'])
         featureDict['body'] = self.tokenize(featureDict['body'])
 
+        # The following feature extraction procedure is rather
+        # inefficient, but since we cannot improve its asymptotic
+        # complexity, it may be worth the tradeoff for readability.
         featureDict['averageWordLength'] = self.getAverageWordLength(featureDict['body'])
         featureDict['ratioAlphaOverTextLength'] = self.getRatioAlphaOverTextLength(featureDict['body'])
+
+        sentences = self.getSentences(featureDict['body'])
+
+        featureDict['numSentences'] = len(sentences)
+        numPunctuationChars = self.getNumPunctuationCharacters(featureDict['body'])
+        try:
+            featureDict['averageSentenceLength'] = len(featureDict['body']) / float(featureDict['numSentences'])
+            featureDict['ratioPunctuationCharactersOverNumSentences'] = numPunctuationChars / float(featureDict['numSentences'])
+        except ZeroDivisionError:
+            featureDict['averageSentenceLength'] = 0
+            featureDict['ratioPunctuationCharactersOverNumSentences'] = 0
+
+        featureDict['YuleK'] = self.getYuleKMeasure(featureDict['body'])
 
         return featureDict
